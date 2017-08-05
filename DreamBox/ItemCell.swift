cf0283833a70e83a5687293bb8df4183321b9bed
manosp3 <emm.perakis@gmail.com>
2017-08-05 13:57:45 +0300

    connect the IBOutlets

---
+++ DreamBox/ItemCell.swift
@@ -0,0 +1,21 @@
+//
+//  ItemCell.swift
+//  DreamBox
+//
+//  Created by Emmanouil Perakis on 05/08/2017.
+//  Copyright © 2017 Emmanouil Perakis. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+
+    @IBOutlet weak var thumb: UIImageView!
+
+    @IBOutlet weak var title: UILabel!
+
+    @IBOutlet weak var price: UILabel!
+    
+    @IBOutlet weak var details: UILabel!
+    
+}
