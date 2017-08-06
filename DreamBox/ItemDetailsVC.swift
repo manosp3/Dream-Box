7b001a891cf991f0d27a2d24ea949b6ad0bb15df
manosp3 <emm.perakis@gmail.com>
2017-08-06 17:56:49 +0300

    ItemDetails screen is created

---
+++ DreamBox/ItemDetailsVC.swift
@@ -0,0 +1,38 @@
+//
+//  ItemDetailsVC.swift
+//  DreamBox
+//
+//  Created by Emmanouil Perakis on 06/08/2017.
+//  Copyright © 2017 Emmanouil Perakis. All rights reserved.
+//
+
+import UIKit
+
+class ItemDetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //ksefortonomaste to keimeno apo to back button otan anoigoume na prosthesoume ena neo item (defteri othoni mas) - ksefortonomaste to tile pou exei vasika
+        if let topItem = self.navigationController?.navigationBar.topItem {
+            topItem.backBarButtonItem = UIBarButtonItem(title: "", style: UIBarButtonItemStyle.plain, target: nil, action: nil)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
